77efb849013d433faff8cb48b570830ca020cc21
Romina <rominahelou@gmail.com>
2025-09-08 10:42:33 +0200

    Changed header title

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="bg-gradient-to-r from-purple-600 to-indigo-500 text-white p-6 shadow-md flex justify-center">
-        <h1 class="text-3xl font-extrabold tracking-wider">✨ Magic Quiz ✨</h1>
+        <h1 class="text-3xl font-extrabold tracking-wider">✨ Sorting Hat Quiz ✨</h1>
     </header>
 </template>
 
