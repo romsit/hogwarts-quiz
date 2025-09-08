6d5359c4cf61a3501fc06dab951fd945d146a601
Romina <rominahelou@gmail.com>
2025-09-08 10:06:40 +0200

    Footer added

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,21 @@
   <div class="home">
     <Header />
     <ChatBot msg="Welcome to Your Vue.js App" />
+    <Footer />
   </div>
 </template>
 
 <script>
 import ChatBot from './components/ChatBot.vue'
 import Header from './components/Header.vue'
+import Footer from './components/Footer.vue';
 
 export default {
   name: 'App',
   components: {
     ChatBot,
-    Header
+    Header, 
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="mt-4 py-2 text-center text-gray-500 text-sm">
+    <p>
+      Made with ❤️ By Romina - 
+       <a
+        href="https://github.com/romsit/hogwarts-quiz"
+        class="underline hover:text-blue-600 transition-colors"
+        target="_blank"
+        rel="noopener noreferrer"
+      >GitHub</a>
+    </p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  border-top: 1px solid #e5e7eb; /* gris claro */
+}
+</style>
